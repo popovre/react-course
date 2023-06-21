7772b8cf37cb6eeda01cf9b54dcedb2b02c72c3e
popovre <popov.roman.e@gmail.com>
2023-06-21 12:45:53 +0500

    fragment

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <React.Fragment>
+      <h1>Roman</h1>
+      <h3>hihihi</h3>
+      <button button>Like!</button>
+    </React.Fragment>
+  );
+};
+
+export default Card;
