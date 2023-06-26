15e2cdbca4637ec94bd048017410ec80474b9b80
popovre <popov.roman.e@gmail.com>
2023-06-26 17:56:03 +0500

    useEffect

--- 20-useEffect/src/App.js
+++ 20-useEffect/src/App.js
@@ -1,15 +1,19 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
   const [todo, setTodo] = useState(null);
-  console.log('app rendered')
+  useEffect(()=>{
+    console.log('called useEffect');
 
-  fetch('https://jsonplaceholder.typicode.com/todos/5')
+    fetch('https://jsonplaceholder.typicode.com/todos/5')
     .then((response) => response.json())
-    // .then((json) => setTodo(json));
+    .then((json) => setTodo(json));
+  }, []);
+  console.log('app rendered')
+  console.log(todo);
 
-  return <div className="App"></div>;
+  return <div className="App"> {todo && <h1>{todo.title}</h1>}</div>
 }
 
 export default App;
