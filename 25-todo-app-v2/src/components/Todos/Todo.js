6fd1dfa1037de77a80a860a5f19e431912687117
Roman <popov.roman.e@gmail.com>
2023-07-06 12:53:01 +0500

    делает удаление туду

--- 25-todo-app-v2/src/components/Todos/Todo.js
+++ 25-todo-app-v2/src/components/Todos/Todo.js
@@ -1,18 +1,18 @@
-import styles from './Todo.module.css'
-import {RiTodoFill, RiDeleteBin2Line} from 'react-icons/ri'
-import {FaCheck} from 'react-icons/fa'
-export default function Todo({todo, deleteTodo}) {
-
-	return (
-		<div className={styles.todo} onDoubleClick={() => {
-			deleteTodo(todo.id);
-		}}> 
-			<RiTodoFill className={styles.todoIcon}/>
-			<div className={styles.todoText}>
-				{todo.text}
-			</div>
-			<RiDeleteBin2Line/>
-			<FaCheck/>
-		</div>
-	)
+import styles from './Todo.module.css';
+import { RiTodoFill, RiDeleteBin2Line } from 'react-icons/ri';
+import { FaCheck } from 'react-icons/fa';
+export default function Todo({ todo, deleteTodo }) {
+  return (
+    <div className={styles.todo}>
+      <RiTodoFill className={styles.todoIcon} />
+      <div className={styles.todoText}>{todo.text}</div>
+      <RiDeleteBin2Line
+        className={styles.deleteIcon}
+        onClick={() => {
+          deleteTodo(todo.id);
+        }}
+      />
+      <FaCheck className={styles.checkIcon} />
+    </div>
+  );
 }
