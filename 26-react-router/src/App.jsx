dccc81de28d8e419f4fd8c5d2317cdcd072375d0
popovre <popov.roman.e@gmail.com>
2023-07-11 16:27:56 +0500

    изучение реакт роутинг

---
+++ 26-react-router/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Contacts from './components/Contacts';
+import About from './components/About';
+import NotFound from './components/NotFound';
+import MainLayout from './layouts/MainLayout';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Home/>}/>
+              <Route path="about" element={<About />} />
+              <Route path="contacts" element={<Contacts />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ 26-react-router/src/components/Menu.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom';
+
+const Menu = () => {
+  return (
+    <nav>
+      <NavLink
+        className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+        to="."
+        end
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+        to="contacts"
+      >
+        Contacts
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+        to="about"
+      >
+        About
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Menu;
